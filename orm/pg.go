2a8433c275e7d27721c6c2bcf5c86ab67143a5c8
Ludwig Muench <muench.ludwig@gmail.com>
2019-02-02 18:03:18 +0100

    get postgres conf from environment variables

--- orm/pg.go
+++ orm/pg.go
@@ -1,6 +1,9 @@
 package orm
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres" // For configuration
 	"github.com/lmuench/godo/models"
@@ -8,12 +11,19 @@ import (
 
 // InitPG automigrates models and returns DB connection pointer
 func InitPG() *gorm.DB {
-	db, err := gorm.Open("postgres",
-		"host=localhost port=5432 dbname=godo_dev user=postgres password=postgres",
+	conf := fmt.Sprintf(
+		"host=%s port=%s dbname=%s user=%s password=%s",
+		os.Getenv("GODO_DEV_DB_HOST"),
+		os.Getenv("GODO_DEV_DB_PORT"),
+		os.Getenv("GODO_DEV_DB_DBNAME"),
+		os.Getenv("GODO_DEV_DB_USER"),
+		os.Getenv("GODO_DEV_DB_PASSWORD"),
 	)
+	db, err := gorm.Open("postgres", conf)
 	if err != nil {
 		panic("failed to connect to database")
 	}
+
 	db.AutoMigrate(&models.Todo{})
 	db.AutoMigrate(&models.User{})
 	return db
