9dfa4243ae9dc02ef84a3367cde5dbfd97d5f353
Ludwig Muench <muench.ludwig@gmail.com>
2019-02-06 17:33:10 +0100

    fetch username from github

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -24,21 +24,21 @@ func (api API) Redirect(w http.ResponseWriter, r *http.Request, _ httprouter.Par
 		return
 	}
 
-	reqURL := fmt.Sprintf(
+	url := fmt.Sprintf(
 		"https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s",
 		os.Getenv("GODO_DEV_GITHUB_OAUTH_CLIENT_ID"),
 		os.Getenv("GODO_DEV_GITHUB_OAUTH_CLIENT_SECRET"),
 		code,
 	)
-	req, err := http.NewRequest(http.MethodPost, reqURL, nil)
+	req, err := http.NewRequest(http.MethodPost, url, nil)
 	if err != nil {
 		respondWithLoginError(w)
 		return
 	}
 	req.Header.Set("accept", "application/json")
 
-	httpClient := http.Client{}
-	res, err := httpClient.Do(req)
+	client := http.Client{}
+	res, err := client.Do(req)
 	if err != nil {
 		respondWithLoginError(w)
 		return
@@ -52,15 +52,49 @@ func (api API) Redirect(w http.ResponseWriter, r *http.Request, _ httprouter.Par
 		return
 	}
 
-	api.cache.Do("SET", token.Value, "1")
+	// api.cache.Do("SET", token.Value, "1")
+	api.cacheTokenAndUsername(w, token)
 
 	w.Header().Set("Location", "/welcome.html?access_token="+token.Value)
 	w.WriteHeader(http.StatusFound)
 }
 
+func (api API) cacheTokenAndUsername(w http.ResponseWriter, token AccessToken) {
+	url := "https://api.github.com/user"
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		respondWithLoginError(w)
+		return
+	}
+	req.Header.Set("accept", "application/json; charset=utf-8")
+	req.Header.Set("Authorization", "token "+token.Value)
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		respondWithLoginError(w)
+		return
+	}
+	defer res.Body.Close()
+
+	var username Username
+	err = json.NewDecoder(res.Body).Decode(&username)
+	if err != nil {
+		respondWithLoginError(w)
+		return
+	}
+
+	api.cache.Do("SET", token.Value, username.Value)
+}
+
 // AccessToken contains OAuth access token
 type AccessToken struct {
 	Value string `json:"access_token"`
+}
+
+// Username contains GitHub (login) username
+type Username struct {
+	Value string `json:"login"`
 }
 
 func respondWithLoginError(w http.ResponseWriter) {
